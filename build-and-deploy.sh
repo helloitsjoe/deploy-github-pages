6d0a587226f94fc232693ce8e1da3a4399b6e3e7
helloitsjoe <jboyle617@gmail.com>
2021-03-10 21:45:09 -0500

    fix

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -30,7 +30,7 @@ yarn build
 if [ "${target_branch}" = "gh-pages" ]; then
   if [ "${build_dir}" != "${target_dir}" ]; then
     echo "Replacing contents of ${target_dir} with ${build_dir}"
-    find "${target_dir}" -type d -not -name "${build_dir}" -delete
+    find "${target_dir}" -type d -not -name "${build_dir}" | xargs -0 -I {} rm -rf {}
     mv -v "${build_dir}/"* "${target_dir}/"
   fi
 fi
