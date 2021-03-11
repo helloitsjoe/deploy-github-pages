f6ab9d630774600dcae1ecf051470d31d3ad59db
helloitsjoe <jboyle617@gmail.com>
2021-03-10 20:09:40 -0500

    fix

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -21,7 +21,7 @@ cd "${GITHUB_WORKSPACE}"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
-git fetch
+git pull
 git checkout "${target_branch}"
 git rebase "${remote_name}/${main_branch}"
 
