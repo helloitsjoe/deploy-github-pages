816d51401434ea5cfc4ac5d4b380ee0ae0788444
helloitsjoe <jboyle617@gmail.com>
2021-03-13 22:06:30 -0500

    env vars

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -2,28 +2,28 @@
 
 set -Ceuo pipefail
 
-github_token=${1}
-remote_name=${2} # origin
-main_branch=${3} # main
-target_branch=${4} # gh-pages
-build_dir=${5} # dist
-target_dir=${6:-$GITHUB_WORKSPACE} # GitHub workspace root
+# github_token=${1}
+# remote_name=${2} # origin
+# main_branch=${3} # main
+# target_branch=${4} # gh-pages
+# build_dir=${5} # dist
+# target_dir=${6:-$GITHUB_WORKSPACE} # GitHub workspace root
 
 echo "Repo: ${GITHUB_REPOSITORY}"
 echo "Workspace: ${GITHUB_WORKSPACE}"
 echo "Actor: ${GITHUB_ACTOR}"
-echo "Build dir: ${build_dir}"
-echo "Target dir: ${target_dir}"
+echo "Build dir: ${BUILD_DIR}"
+echo "Target dir: ${TARGET_DIR}"
 
-repo_uri="https://x-access-token:${github_token}@github.com/${GITHUB_REPOSITORY}.git"
+repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-cd "${GITHUB_WORKSPACE}"
+# cd "${GITHUB_WORKSPACE}"
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
-if [ "${target_dir}" = "${GITHUB_WORKSPACE}" ]; then
-  if [ "${target_branch}" = "gh-pages" ]; then
+if [ "${TARGET_BRANCH}" = "gh-pages" ]; then
+  if [ "${TARGET_DIR}" = "${GITHUB_WORKSPACE}" ]; then
     # Only push subtree if we're on gh-pages
     echo "gh-pages, pushing subtree..."
     # git checkout "${main_branch}"
@@ -31,7 +31,7 @@ if [ "${target_dir}" = "${GITHUB_WORKSPACE}" ]; then
     yarn build
 
     echo 'Staging changes...'
-    git add "${target_dir}"
+    git add "${TARGET_DIR}"
 
     set +e
     git status
@@ -42,34 +42,34 @@ if [ "${target_dir}" = "${GITHUB_WORKSPACE}" ]; then
     fi
     set -e
 
-    git remote set-url "${remote_name}" "${repo_uri}"
-    git push "${remote_name}" `git subtree split --prefix ${build_dir}`:gh-pages --force
+    git remote set-url "${REMOTE_NAME}" "${REPO_URI}"
+    git push "${REMOTE_NAME}" `git subtree split --prefix ${BUILD_DIR}`:gh-pages --force
     # git subtree push --prefix "${build_dir}" origin gh-pages
     echo 'Pushed subtree, exiting...'
     exit 0
   fi
 fi
 
-git checkout "${target_branch}"
-git rebase "${remote_name}/${main_branch}"
+git checkout "${TARGET_BRANCH}"
+git rebase "${REMOTE_NAME}/${MAIN_BRANCH}"
 
 yarn --frozen-lockfile
 yarn build
 
-if [ "${build_dir}" != "${target_dir}" ]; then
-  if [ "${target_dir}" = "${GITHUB_WORKSPACE}" ]; then
-    echo "Moving contents of ${build_dir} to ${target_dir}"
-    mv -v "${build_dir}/"* "${target_dir}/"
+if [ "${BUILD_DIR}" != "${TARGET_DIR}" ]; then
+  if [ "${TARGET_DIR}" = "${GITHUB_WORKSPACE}" ]; then
+    echo "Moving contents of ${BUILD_DIR} to ${TARGET_DIR}"
+    mv -v "${BUILD_DIR}/"* "${TARGET_DIR}/"
   else
-    echo "Renaming ${build_dir} to ${target_dir}"
-    mv -v "${build_dir}" "${target_dir}"
+    echo "Renaming ${BUILD_DIR} to ${TARGET_DIR}"
+    mv -v "${BUILD_DIR}" "${TARGET_DIR}"
   fi
 else
   echo "Build and target dirs are the same, continuing..."
 fi
 
 echo 'Staging changes...'
-git add "${target_dir}"
+git add "${TARGET_DIR}"
 
 set +e
 git status
@@ -80,5 +80,5 @@ if [ $? -ne 0 ]; then
 fi
 set -e
 
-git remote set-url "${remote_name}" "${repo_uri}"
-git push --force-with-lease "${remote_name}" "${target_branch}"
+git remote set-url "${REMOTE_NAME}" "${REPO_URI}"
+git push --force-with-lease "${REMOTE_NAME}" "${TARGET_BRANCH}"
