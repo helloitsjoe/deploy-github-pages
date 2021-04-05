06d27c176e65da12fe744ab1505524ef65729b98
helloitsjoe <jboyle617@gmail.com>
2021-04-04 22:58:58 -0400

    testing

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -24,8 +24,10 @@ cd "${GITHUB_WORKSPACE}"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
+set +e
 branch_exists=$(git branch --remote | grep -w "${TARGET_BRANCH}")
 echo "Branch exists: ${branch_exists}"
+set -e
 
 if [[ $branch_exists ]]; then
   echo 'Branch exists, continuing...'
