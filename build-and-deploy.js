7b32ee65dbc63d853789a15dcdfddaf3ae54bf6e
helloitsjoe <jboyle617@gmail.com>
2021-04-17 15:51:44 -0400

    test

--- build-and-deploy.js
+++ build-and-deploy.js
@@ -4,14 +4,14 @@ const gh = require('@actions/github');
 console.log(`gh:`, gh);
 console.log(`context:`, gh.context);
 
-const GITHUB_WORKSPACE = 'foo'
+const GITHUB_WORKSPACE = 'foo';
 const TARGET_DIR = core.getInput('target_dir') || GITHUB_WORKSPACE;
 const MAIN_BRANCH = core.getInput('main_branch');
 const TARGET_BRANCH = core.getInput('target_branch');
 const BUILD_DIR = core.getInput('build_dir');
 const BRANCH_BUILD = core.getInput('branch_build');
 
-const GITHUB_ACTOR
+// const GITHUB_ACTOR
 
 console.log(`TARGET_DIR: ${TARGET_DIR}`);
 
@@ -27,5 +27,5 @@ console.log(`Branch build: ${BRANCH_BUILD}`);
 
 process.chdir(GITHUB_WORKSPACE);
 
-fs.execSync(`git config user.name ${GITHUB_ACTOR}`);
-fs.execSync(`git config user.email ${GITHUB_ACTOR}@bots.github.com`);
+// fs.execSync(`git config user.name ${GITHUB_ACTOR}`);
+// fs.execSync(`git config user.email ${GITHUB_ACTOR}@bots.github.com`);
